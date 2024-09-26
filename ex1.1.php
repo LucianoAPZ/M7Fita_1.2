ab3fb518400d368618f1ff7e49a8d90ea6940c9e
LucianoAPZ <lucianoanpoza@gmail.com>
2024-09-26 18:24:59 +0200

    primera subida fita2

---
+++ ex1.1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fita2 -ex1</title>
+</head>
+<body>
+	<form>
+		<label for="userName">User: </label>
+		<input type="text" name="userName" placeholder="Pepe123">
+
+		<label for="userPassword">Password: </label>
+		<input type="password" name="userPassword">
+
+		<input type="submit" name="">
+	</form>
+</body>
+</html>
